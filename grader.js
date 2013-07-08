d52dbcae2d7b99904bda7ad82dd38b00547da4b7
Michael Bodell <mbodell@gmail.com>
2013-07-08 08:31:54 +0000

    Made changes to use restler

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,12 +42,28 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var str = "";
+
+var loadUrlContent = function(result) {
+    if(result instanceof Error) {
+        console.log("Error while loading url: %s. Exiting.", result.message) 
+        process.exit(2);
+    } else {
+        str = result;
+    }
+};
+
+var cheerioHtmlLoad = function(htmlfile) {
+    rest.get(htmlfile).on('complete', loadUrlContent);
+    return cheerio.load(str);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, checkfunction) {
+    $ = checkfunction(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,8 +83,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Url of index.html', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkFn = cheerioHtmlFile;
+    var checkFile = program.file;
+    if (program.url.length > 0) {
+        checkFile = program.url;
+        checkFn = cheerioHtmlLoad;
+    }
+    var checkJson = checkHtmlFile(checkFile, program.checks, checkFn);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
